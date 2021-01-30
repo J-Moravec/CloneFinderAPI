e5d7888c7d9cd9ca231aec371d2258d8d2f523f9
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-01-30 13:10:22 +1300

    Changed package name to clonefinder

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@ with open("README.txt", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='CloneFinderAPI',
+    name="clonefinder",
     version="0.1",
 
-    author='Sayaka Miura',
-    author_email='tuf78332@temple.edu',
+    author="Sayaka Miura",
+    author_email="tuf78332@temple.edu",
 
-    description='Estimate clone genotypes and frequencies within a tumor sample using a phylogenetic approach.',
+    description="Estimate clone genotypes and frequencies within a tumor sample using a phylogenetic approach.",
     long_description=long_description,
-    long_description_content_type='text/plain',
-    url='https://github.com/gstecher/CloneFinderAPI',
+    long_description_content_type="text/plain",
+    url="https://github.com/gstecher/CloneFinderAPI",
 
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         ],
 
-    python_requires='>=3',
-    install_requires=['numpy', 'scipy', 'biopython'],
+    python_requires=">=3",
+    install_requires=["numpy", "scipy", "biopython"],
     packages = setuptools.find_packages(),
 
     entry_points = {
-        'console_scripts' : [
-            'clonefinder = clonefinder.clonefinder:main',
+        "console_scripts" : [
+            "clonefinder = clonefinder.clonefinder:main",
             ],
         },
     )
